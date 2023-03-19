cafd31e090275e7efec327d49ea3c32ab0268c25
Kyle Finlinson <5882840+KyleFin@users.noreply.github.com>
2023-03-18 22:04:16 -0600

    Enable watch support

--- wear_os_bible_ereader/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++ wear_os_bible_ereader/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.verygoodcore.wear_os_bible_ereader
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.annotation.NonNull
+import android.view.MotionEvent
+import com.samsung.wearable_rotary.WearableRotaryPlugin
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+    override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
+        return when {
+            WearableRotaryPlugin.onGenericMotionEvent(event) -> true
+            else -> super.onGenericMotionEvent(event)
+        }
+    }
 }
