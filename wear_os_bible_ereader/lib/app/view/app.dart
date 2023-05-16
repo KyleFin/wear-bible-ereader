72b56c1d4e92c55d502a6ab3799212eed9fcd601
Kyle Finlinson <5882840+KyleFin@users.noreply.github.com>
2023-05-15 22:15:31 -0600

    Flowbuilder, cubit, epubController working

--- wear_os_bible_ereader/lib/app/view/app.dart
+++ wear_os_bible_ereader/lib/app/view/app.dart
@@ -1,9 +1,46 @@
+import 'package:epub_view/epub_view.dart';
+import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wear_os_bible_ereader/counter/counter.dart';
 import 'package:wear_os_bible_ereader/l10n/l10n.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  List<Page> onGeneratePages(PositionCubitState state, List<Page> pages) {
+    final selectedBook = state.bookTitle;
+    return [
+      BookshelfPage.page(),
+      if (selectedBook != null)
+        BookDetailsPage.page(epubController: _epubReaderController)
+    ];
+  }
+
+  late EpubController _epubReaderController;
+
+  @override
+  void initState() {
+    _epubReaderController = EpubController(
+      document: EpubDocument.openAsset('assets/kjvCh.epub'),
+      // epubCfi:
+      //     'epubcfi(/6/26[id4]!/4/2/2[id4]/22)', // book.epub Chapter 3 paragraph 10
+      // epubCfi:
+      //     'epubcfi(/6/6[chapter-2]!/4/2/1612)', // book_2.epub Chapter 16 paragraph 3
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _epubReaderController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +56,79 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: BlocListener<PositionCubit, PositionCubitState>(
+        listenWhen: (previous, current) {
+          final currBookFilename = titleToFilename[current.bookTitle];
+          return currBookFilename != null &&
+              // TODO Verify with tests (did with breakpoints).
+              // Only change controller if opening a different book.
+              currBookFilename != current.latestBook;
+        },
+        listener: (context, state) {
+          final bookFilename = titleToFilename[state.bookTitle]!;
+          context.read<PositionCubit>().setLatestBook(bookFilename);
+          _epubReaderController.dispose();
+          _epubReaderController = EpubController(
+            document: EpubDocument.openAsset(
+              'assets/$bookFilename',
+            ),
+          );
+          // ..loadingState.addListener(() {
+          //     context.read<PositionCubit>().setBookIsLoading(
+          //           _epubReaderController.loadingState.value ==
+          //               EpubViewLoadingState.loading,
+          //         );
+          //   });
+          // final cubit = context.read<PositionCubit>()..setBookIsLoading(true);
+          // _epubReaderController.;
+        },
+        child: FlowBuilder(
+          state: context.watch<PositionCubit>().state,
+          onGeneratePages: onGeneratePages,
+        ),
+      ),
+      // home: const CounterPage(),
+    );
+  }
+}
+
+class BookDetailsPage extends StatelessWidget {
+  const BookDetailsPage({required this.epubController, super.key});
+
+  static Page page({required EpubController epubController}) {
+    return MaterialPage<void>(
+      child: BookDetailsPage(epubController: epubController),
+    );
+  }
+
+  final EpubController epubController;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return WillPopScope(
+      onWillPop: () async {
+        await context.read<PositionCubit>().closeBook();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Details')),
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: EpubView(
+            //TableOfContents(
+            controller: epubController,
+            // itemBuilder: (context, index, chapter, itemCount) =>
+            //     chapter.type == 'chapter'
+            //         ? ListTile(
+            //             title: Text(chapter.title!.trim()),
+            //             onTap: () =>
+            //                 epubController.scrollTo(index: chapter.startIndex),
+            //           )
+            //         : const SizedBox.shrink(),
+          ),
+        ),
+      ),
     );
   }
 }
--- wear_os_bible_ereader/lib/counter/view/counter_page.dart
+++ wear_os_bible_ereader/lib/counter/view/counter_page.dart
@@ -1,98 +1,57 @@
 import 'package:epub_view/epub_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:wear_os_bible_ereader/counter/counter.dart';
 import 'package:wear_os_bible_ereader/epub/epub.dart';
 import 'package:wear_os_bible_ereader/l10n/l10n.dart';
 
-class CounterPage extends StatelessWidget {
-  const CounterPage({super.key});
+class BookshelfPage extends StatelessWidget {
+  const BookshelfPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => PositionCubit(),
-      child: const CounterView(),
-    );
+  static Page page() {
+    return const MaterialPage<void>(child: BookshelfPage());
   }
-}
-
-class CounterView extends StatelessWidget {
-  const CounterView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // final l10n = context.l10n;
-    return const Scaffold(body: NavigationList()
-        // EpubWidget(
-        //   filename: 'bofm.epub',
-        // ),
-        //     SizedBox.expand(
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     children: [
-        //       ElevatedButton(
-        //         onPressed: () => context.read<PositionCubit>(), //.increment(),
-        //         child: const Icon(Icons.add),
-        //       ),
-        //       const SizedBox(height: 10),
-        //       Text(l10n.libraryAppBarTitle),
-        //       const CounterText(),
-        //       const SizedBox(height: 10),
-        //       ElevatedButton(
-        //         onPressed: () => context.read<PositionCubit>(), //.decrement(),
-        //         child: const Icon(Icons.remove),
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        );
-  }
-}
-
-class NavigationList extends StatelessWidget {
-  const NavigationList({super.key});
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return BlocBuilder<PositionCubit, PositionCubitState>(
-      builder: (context, state) {
-        final cubit = context.read<PositionCubit>();
-        return state.bookTitle == null
-            ? ListView(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  for (final k in titleToFilename.keys)
-                    ElevatedButton(
-                      onPressed: () => cubit.openBook(k),
-                      child: Text(k),
-                    ),
-                  const SizedBox(height: 10),
-                  Text(l10n.libraryAppBarTitle),
-                  const CounterText(),
-                  const SizedBox(height: 10),
-                ],
-              )
-            : state.bookIsLoading
-                ? const Text('Loading')
-                :
-                // EpubViewTableOfContents()
-                ListView(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (final c in cubit.chapters())
-                        ElevatedButton(
-                          onPressed: () => cubit.openBook(c),
-                          child: Text(c),
-                        ),
-                      const SizedBox(height: 10),
-                      Text(l10n.libraryAppBarTitle),
-                      const CounterText(),
-                      const SizedBox(height: 10),
-                    ],
-                  );
-      },
+    final cubit = context.read<PositionCubit>();
+    return //state.bookTitle == null ?
+        Scaffold(
+      body: ListView(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (final k in titleToFilename.keys)
+            ElevatedButton(
+              onPressed: () => cubit.openBook(k),
+              child: Text(k),
+            ),
+          const SizedBox(height: 10),
+          Text(l10n.libraryAppBarTitle),
+          const CounterText(),
+          const SizedBox(height: 10),
+        ],
+        // ),
+        // : state.bookIsLoading
+        //     ? const Text('Loading')
+        //     :
+        //     // EpubViewTableOfContents()
+        //     ListView(
+        //         // mainAxisAlignment: MainAxisAlignment.center,
+        //         children: [
+        //           for (final c in cubit.chapters())
+        //             ElevatedButton(
+        //               onPressed: () => cubit.openBook(c),
+        //               child: Text(c),
+        //             ),
+        //           const SizedBox(height: 10),
+        //           Text(l10n.libraryAppBarTitle),
+        //           const CounterText(),
+        //           const SizedBox(height: 10),
+        //         ],
+        //       );
+      ),
     );
   }
 }
