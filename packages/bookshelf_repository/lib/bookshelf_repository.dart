4990a712f35bc12e2918bc5ea666262389e3ff38
Kyle Finlinson <kyle.finlinson@verygood.ventures>
2023-07-11 15:47:45 -0600

    Create BookshelfRepository

---
+++ packages/bookshelf_repository/lib/bookshelf_repository.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library bookshelf_repository;
+
+export 'src/bookshelf_repository.dart';
---
+++ packages/bookshelf_repository/lib/src/bookshelf_repository.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+/// {@template bookshelf_repository}
+/// Manages synchronizing books between companion and Wear OS apps.
+/// {@endtemplate}
+abstract class BookshelfRepository {
+  /// {@macro bookshelf_repository}
+  const BookshelfRepository();
+}
